6ed7eaaffc54ec2355a82910258a7c30be443d17
belcortes <belcortes@gmail.com>
2018-03-19 16:35:54 -0700

    first commit

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,15 @@
+INSERT INTO USERS
+  (USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+  ('user from seeds', 'Ima', 'User'),
+  ('another user', 'Ura', 'User'),
+  ('someone', 'Heeza', 'User'),
+  ('else', 'Sheeza', 'User');
+
+INSERT INTO SONGS
+    (TITLE, SONG_LENGTH, ARTIST)
+VALUES
+    ('Black Tambourine', '2:47', 'Beck'),
+    ('Starman', '4:19', 'David Bowie'),
+    ('Can You Get To That', '2:51', 'Funkadelic'),
+    ('Peaceland', '8:00', 'Nujabes');
